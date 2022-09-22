750a6f8269ee5a9ec02c5450935749a8e2a96fac
medyolangs <medyochain@gmail.com>
2022-09-22 16:52:17 +0800

    add new test

--- src/test/survey.test.js
+++ src/test/survey.test.js
@@ -21,4 +21,8 @@ describe("Survey Model", function(){
 
         expect(result).to.equal("Error! Captcha input doesn't matched.");
     });
+
+    it('Should return 1 + 1 = 2', () => {
+        expect(1+1).to.equal(2);
+    });
 });
